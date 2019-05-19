81c53ada4b92f411b8cbddfb48d446dac3e8206e
SukruthRam <SukruthRam@users.noreply.github.com>
2019-05-20 00:51:41 +0200

    Initial Commit

---
+++ rabbitmq-amqp-tutorials/src/main/java/org/springframework/amqp/tutorials/RabbitAmqpTutorialsRunner.java
@@ -0,0 +1,26 @@
+package org.springframework.amqp.tutorials;
+
+import org.springframework.amqp.tutorials.tut1.Tut1Sender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class RabbitAmqpTutorialsRunner implements CommandLineRunner {
+
+    @Value("${tutorial.client.duration:0}")
+    private int duration;
+
+    @Autowired
+    private ConfigurableApplicationContext ctx;
+
+    Tut1Sender aTut1Sender = new Tut1Sender();
+    
+    @Override
+    public void run(String... arg0) throws Exception {
+        System.out.println("Ready ... running for " + duration + "ms");
+        
+        Thread.sleep(duration);
+        ctx.close();
+    }
+}
